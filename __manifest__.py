794b132faa19b13f6b60555bab2955e289ab4e15
He Jian <hej@eabax.com>
2024-04-02 10:16:01 +0800

    fix

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'cn Accounting Reports',
+    'author': "He Jian",
+    'category': 'Accounting/Localizations/Reporting',
+    'depends': [
+        'l10n_cn_standard',
+        'account_reports',
+    ],
+    'data': [
+        'data/profit_and_loss.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
